423460fc0e386a48575eddc11ee83d5ab0f4755c
benhoe2000 <benhoe2000@gmail.com>
2019-01-15 21:34:03 +0800

    Commit

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get("/test", function (req, res) {
 })
 
 
-var listener = app.listen(process.env.PORT,process.env.IP,function(){
+
 //var listener = app.listen(4000, process.env.IP, function () {
     var listener = app.listen(process.env.PORT,process.env.IP,function(){
     console.log("server has started");
